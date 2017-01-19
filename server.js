abd01aa14b68b9c5abaefd883200fc9917f36c91
njuelle <n.juelle@gmail.com>
2017-01-19 12:58:08 +0100

    set password mongo

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var routes = require('./routes');
 var mongoose       = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/3bdb');
-mongoose.connect('mongodb://association3B:6^HJUC&D>c4^r_s@localhost:27017/3bdb');
+mongoose.connect('mongodb://localhost/test',{user: 'association3B', pass: '6^HJUC&D>c4^r_s)'},callback);
+                 
+                 
 
 // Create our Express application
 var app = express();
