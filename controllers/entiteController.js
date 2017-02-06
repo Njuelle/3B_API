fe73ec0c0a94db9697a1caaffec42743018c03c2
njuelle <n.juelle@gmail.com>
2017-02-06 16:25:11 +0100

    add many routes for ENTITE

--- controllers/entiteController.js
+++ controllers/entiteController.js
@@ -25,60 +25,131 @@ module.exports = {
     },
 
     getVoyagePersonnel : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'voyages_personels', req, res);
+        crudController.getObjectChild(Entite, 'infos_asso.voyages_personels', req, res);
     },
 
     putVoyagePersonnel : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'voyages_personels', req, res);
+        crudController.putObjectChild(Entite, 'infos_asso.voyages_personels', req, res);
     },
 
     getAutreAsso : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'autre_association', req, res);
+        crudController.getObjectChild(Entite, 'infos_asso.autre_association', req, res);
     },
 
     putAutreAsso : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'autre_association', req, res);
+        crudController.putObjectChild(Entite, 'infos_asso.autre_association', req, res);
     },
 
     getCompetences : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'competences', req, res);
+        crudController.getObjectChild(Entite, 'infos_asso.competences', req, res);
     },
 
     putCompetences : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'competences', req, res);
+        crudController.putObjectChild(Entite, 'infos_asso.competences', req, res);
     },
 
     getAdresse : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'adresse', req, res);
+        crudController.getObjectChild(Entite, 'adresse', req, res);
     },
 
     putAdresse : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'adresse', req, res);
+        crudController.putObjectChild(Entite, 'adresse', req, res);
     },
 
     getEtatCivil : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'etat_civil', req, res);
+        crudController.getObjectChild(Entite, 'etat_civil', req, res);
     },
 
     putEtatCivil : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'etat_civil', req, res);
+        crudController.putObjectChild(Entite, 'etat_civil', req, res);
     },
 
     getContact : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'contact', req, res);
+        crudController.getObjectChild(Entite, 'contact', req, res);
     },
 
     putContact : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'contact', req, res);
+        crudController.putObjectChild(Entite, 'contact', req, res);
     },
 
     getRelationAsso : function(req, res) {
-        crudController.getObjectSubDoc(Entite, 'relation', req, res);
+        crudController.getObjectChild(Entite, 'relation', req, res);
     },
 
     putRelationAsso : function(req, res) {
-        crudController.putObjectSubDoc(Entite, 'relation', req, res);
+        crudController.putObjectChild(Entite, 'relation', req, res);
     },
 
+    getAdministration : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.administration', req, res);
+    },
+
+    putAdministration : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.administration', req, res);
+    },
+
+    getMobilite : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.mobilite', req, res);
+    },
+
+    putMobilite : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.mobilite', req, res);
+    },
+
+    getSanitaire : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.sanitaire', req, res);
+    },
+
+    putSanitaire : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.sanitaire', req, res);
+    },
+
+    getPere : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.parents.pere', req, res);
+    },
+
+    putPere : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.parents.pere', req, res);
+    },
+
+    getMere : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.parents.mere', req, res);
+    },
+
+    putMere : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.parents.mere', req, res);
+    },
+
+    getRepLegal : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.parents.responsable_legal_autre', req, res);
+    },
+
+    putRepLegal : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.parents.responsable_legal_autre', req, res);
+    },
+
+    getScolarite : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.scolarite', req, res);
+    },
+
+    putScolarite : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.scolarite', req, res);
+    },
+
+    getLangueVivante : function(req, res) {
+        crudController.getObjectChild(Entite, 'infos_asso.langues_vivantes', req, res);
+    },
+
+    putLangueVivante : function(req, res) {
+        crudController.putObjectChild(Entite, 'infos_asso.langues_vivantes', req, res);
+    },
+
+    getGroupe : function(req, res) {
+        crudController.getObjectChild(Entite, 'common.groupe', req, res);
+    },
+
+    putGroupe : function(req, res) {
+        crudController.putObjectChild(Entite, 'common.groupe', req, res);
+    
 }
 
